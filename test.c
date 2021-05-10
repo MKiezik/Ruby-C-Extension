007579a5d148f8b4031f3233f0cc57fedda0af1c
Michal Kiezik <michal.kiezik@gmail.com>
2021-05-10 10:16:59 -0700

    integer test

--- test.c
+++ test.c
@@ -2,6 +2,7 @@
 
 // functions declarations
 VALUE string_test();
+VALUE integer_test();
 
 
 // Init function name must be followed with 
@@ -9,6 +10,7 @@ VALUE string_test();
 void Init_test() {
 
   rb_define_global_function("string_test", string_test, 0);
+  rb_define_global_function("integer_test", integer_test, 0);
 }
 
 
@@ -16,8 +18,9 @@ VALUE string_test() {
 
   // in C it would look like int *a = "abcd", but in ruby everythig is VALUE
 
+// we can use 'literal' instead of 'cstr', string blinks when 'puts'
   VALUE abcd = rb_str_new_cstr("Abcd");
-  VALUE abcd2 = rb_str_new_literal("\e[5mRUBY\e[0m"); // we can use 'literal' instead of 'cstr', string blinks when 'puts'
+  VALUE abcd2 = rb_str_new_literal("\e[5mRUBY\e[0m"); 
   rb_p(abcd);
 
   // when we want to pass length of String
@@ -35,3 +38,46 @@ VALUE string_test() {
   return rb_str_new_cstr("string_test method");
 }
 
+VALUE integer_test() {
+
+  // In irb console 250.object_id == 501
+  // VALUE numx = 28;
+  // rb_p(numx); // prints 250 instead of 501, 28 dumps core. Stay away from this declaration
+
+  VALUE num = INT2FIX(501);
+  rb_p(num);
+  VALUE big_num = INT2NUM(2147483647);
+  rb_p(big_num);
+  VALUE u_big_num = UINT2NUM(4294967295U);
+  rb_p(u_big_num);
+
+  int a = 5;
+  VALUE av = INT2FIX(a);
+  rb_p(av);
+  unsigned int b = 6;
+  VALUE bv1 = INT2NUM(b);
+  VALUE bv2 = INT2FIX(b);
+  VALUE bv3 = UINT2NUM(b); // there is no UINT2FIX 
+
+  rb_p(bv1);
+  rb_p(bv2);
+  rb_p(bv3);
+
+  long c = 7;
+  VALUE cv = LONG2NUM(c);
+  rb_p(cv);
+
+  unsigned long d = 8;
+  VALUE dv = ULONG2NUM(d);
+  rb_p(dv);
+
+  unsigned long long e = 9;
+  VALUE ev = ULL2NUM(e);
+  rb_p(ev);
+
+  long long f = 9;
+  VALUE fv = LL2NUM(f);
+  rb_p(fv);
+
+  return rb_str_new_cstr("integer_test method");
+}
