4cb7adc84dd7f6e44058fad303b36e42d0a3edfb
mkiezik <m.kiezik@intratel.pl>
2021-05-10 12:42:44 +0200

    creating global String variable

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 ruby extconf.rb
 make
+
+# Console
+
+$ ruby -r "./test.so" -e 'p $a'
+or
+$ irb
+  : require_relative './test'
+  : $a
+  : $a.class
--- test.c
+++ test.c
@@ -3,5 +3,5 @@
 // Init function name must be followed with 
 // the name from extconf.rb file in 'create_makefile' method
 void Init_test() {
-
+  rb_gv_set("$a", rb_str_new_cstr("abcd")); // global String variable, rb_str_new_cstr(*char)
 }
